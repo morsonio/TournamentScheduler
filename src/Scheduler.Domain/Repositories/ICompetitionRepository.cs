4bd710cbef01248f4d9191216a72787b000052b1
morsonio <siejka.eryk@gmail.com>
2025-08-24 20:24:51 +0200

    .Changed Naming convention, added some required repos by use cases

---
+++ src/Scheduler.Domain/Repositories/ICompetitionRepository.cs
@@ -0,0 +1,10 @@
+﻿using Scheduler.Domain.Entities;
+
+namespace Scheduler.Domain.Repositories
+{
+    internal interface ICompetitionRepository
+    {
+        Task<IEnumerable<Competition>> GetAllAsync();
+        Task<Competition> GetByIdWithTeamsAsync(int competitionId);
+    }
+}
---
+++ src/Scheduler.Domain/Repositories/IScheduleRepository.cs
@@ -0,0 +1,14 @@
+﻿using Scheduler.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler.Domain.Repositories
+{
+    internal interface IScheduleRepository
+    {
+        Task<Schedule> GetByCompetitionIdWithMatchesAsync(int competitionId);
+    }
+}
